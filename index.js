bb36361acae240def277124715f456de3eaa4e7d
Ibenka4415 <91208718+Ibenka4415@users.noreply.github.com>
2024-02-25 22:06:15 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 var http = require('http');
 var url = require('url');
 var dt = require('./datetime');
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/roll') {
+        const result = Math.floor(Math.random() * 6) + 1;
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end(`You rolled a ${result}`);
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('404 Not Found');
+    }
+});
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});
 
 
 const server = http.createServer((request, response) => {
